f9dc7ca6b7e527d504a2b69b45e0ecff42b485a6
PaulB-H <p.bernardhall@gmail.com>
2021-07-22 23:15:22 -0400

    index.js: add creation of backup dir

--- index.js
+++ index.js
@@ -16,3 +16,13 @@ let hasVisited;
 let lastVisited;
 
 let analyticDataArr = [];
+
+let backupPath = path.join(__dirname, "analytic_backups");
+
+if (!fs.existsSync(backupPath)) {
+  console.log("dir not found, creating...");
+  fs.mkdirSync(backupPath);
+  console.log(`analytic_backups dir created at ${backupPath}`);
+} else {
+  console.log(`analytic_backups dir already exists at ${backupPath}`);
+}
