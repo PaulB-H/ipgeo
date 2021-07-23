4a2e37a8d84f5b4d00b2b65513ec6882c0a50a88
PaulB-H <p.bernardhall@gmail.com>
2021-07-23 12:09:45 -0400

    index.js: remove root path to static file
    This is served automatically when using express.static if there is an index.html in the static dir you have defined

--- index.js
+++ index.js
@@ -50,10 +50,6 @@ if (!fs.existsSync(backupPath)) {
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "404.html"));
 });
