529606c266d68da2541996e24c2d465975623bea
PaulB-H <p.bernardhall@gmail.com>
2021-09-07 11:52:23 -0400

    public/script: add call to /analytics

--- public/script.js
+++ public/script.js
@@ -2,3 +2,21 @@ const sendScreenSize = () => {
   axios.get(`/screensize/${window.innerWidth}/height/${window.innerHeight}`);
 };
 sendScreenSize();
+
+const fetchAnalytics = () => {
+  fetch("/analytics")
+    .then((res) => {
+      if (res.status !== 200) {
+        return console.log(`err: ${res.status}`);
+      }
+
+      res.json().then((data) => {
+        console.log(data);
+      });
+    })
+    .catch((err) => {
+      console.log("Fetch Error :-S", err);
+    });
+};
+
+fetchAnalytics();
