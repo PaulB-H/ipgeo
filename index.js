097c08e8d81e806daaab90299f21326a2dba4801
PaulB-H <51964537+PaulB-H@users.noreply.github.com>
2021-10-15 23:47:26 -0400

    Moved routes into router module

--- index.js
+++ index.js
@@ -18,96 +18,9 @@ app.use(sessionManagement);
 
 app.use(express.static(path.join(__dirname, "public"), { index: false }));
 
-app.get("/", (req, res) => {
-  // const clientIp = requestIp.getClientIp(req); // FOR PROD
-  const clientIp = process.env.TEST_IPADDRESS; // FOR DEV
+const routes = require("./routes");
 
-  logResourceRequest(clientIp, "/");
-
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.get("/screensize/:width/height/:height", (req, res) => {
-  // console.log("screensize route hit");
-
-  if (req.params.width % 1 !== 0 || req.params.height % 1 !== 0) {
-    // console.log("Screen size cannot be decimal");
-    res.end();
-  }
-
-  if (req.params.width <= 0 || req.params.height <= 0) {
-    // console.log("Screen size cannot be negative");
-    res.end();
-  }
-
-  if (req.params.width > 10000 || req.params.height > 10000) {
-    // console.log("Screen size cannot be > 10000");
-    res.end();
-  }
-
-  // const clientIp = requestIp.getClientIp(req); // FOR PROD
-  const clientIp = process.env.TEST_IPADDRESS; // FOR DEV
-
-  const existingSession = todaysAnalyticObj.activeSessions.find(
-    (item) => item.ip === clientIp
-  );
-
-  if (existingSession) {
-    logResourceRequest(clientIp, "/screensize");
-  }
-
-  if (
-    existingSession &&
-    existingSession.initialScreenWidth === null &&
-    existingSession.initialScreenHeight === null
-  ) {
-    // console.log("Existing session with no screen size found");
-    existingSession.initialScreenWidth = req.params.width;
-    existingSession.initialScreenHeight = req.params.height;
-  }
-
-  res.end();
-});
-
-app.get("/analytics", (req, res) => {
-  class PublicAnalyticObj {
-    constructor() {
-      this.date = todaysAnalyticObj.date;
-      this.uniqueVisitors = todaysAnalyticObj.iplog.length;
-      this.countries = todaysAnalyticObj.countries;
-      this.totalSessions =
-        todaysAnalyticObj.activeSessions.length +
-        todaysAnalyticObj.closedSessions.length;
-    }
-  }
-
-  const analyticsToSend = new PublicAnalyticObj();
-
-  res.json(analyticsToSend);
-});
-
-app.get("*", (req, res) => {
-  // console.log("404 hit");
-
-  let reqUrl;
-
-  if (req.url.length > 25) {
-    // console.log("req.url longer than 25 chars, truncating");
-
-    for (let i = 1; i < 25; i++) {
-      reqUrl += req.url.charAt(i);
-    }
-  } else {
-    reqUrl = req.url;
-  }
-
-  // const clientIp = requestIp.getClientIp(req); // FOR PROD
-  const clientIp = process.env.TEST_IPADDRESS; // FOR DEV
-
-  logResourceRequest(clientIp, "404 - " + reqUrl);
-
-  res.sendFile(path.join(__dirname, "public", "404.html"));
-});
+app.use(routes);
 
 app.listen(8090, () => {
   console.log("server up on 8090");
---
+++ routes.js
@@ -0,0 +1,99 @@
+const path = require("path");
+const express = require("express");
+const router = express.Router();
+
+let { todaysAnalyticObj } = require("./analytic_main");
+const { logResourceRequest } = require("./analytic_main");
+
+router.get("/", (req, res) => {
+  // const clientIp = requestIp.getClientIp(req); // FOR PROD
+  const clientIp = process.env.TEST_IPADDRESS; // FOR DEV
+
+  logResourceRequest(clientIp, "/");
+
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+router.get("/screensize/:width/height/:height", (req, res) => {
+  // console.log("screensize route hit");
+
+  if (req.params.width % 1 !== 0 || req.params.height % 1 !== 0) {
+    // console.log("Screen size cannot be decimal");
+    res.end();
+  }
+
+  if (req.params.width <= 0 || req.params.height <= 0) {
+    // console.log("Screen size cannot be negative");
+    res.end();
+  }
+
+  if (req.params.width > 10000 || req.params.height > 10000) {
+    // console.log("Screen size cannot be > 10000");
+    res.end();
+  }
+
+  // const clientIp = requestIp.getClientIp(req); // FOR PROD
+  const clientIp = process.env.TEST_IPADDRESS; // FOR DEV
+
+  const existingSession = todaysAnalyticObj.activeSessions.find(
+    (item) => item.ip === clientIp
+  );
+
+  if (existingSession) {
+    logResourceRequest(clientIp, "/screensize");
+  }
+
+  if (
+    existingSession &&
+    existingSession.initialScreenWidth === null &&
+    existingSession.initialScreenHeight === null
+  ) {
+    // console.log("Existing session with no screen size found");
+    existingSession.initialScreenWidth = req.params.width;
+    existingSession.initialScreenHeight = req.params.height;
+  }
+
+  res.end();
+});
+
+router.get("/analytics", (req, res) => {
+  class PublicAnalyticObj {
+    constructor() {
+      this.date = todaysAnalyticObj.date;
+      this.uniqueVisitors = todaysAnalyticObj.iplog.length;
+      this.countries = todaysAnalyticObj.countries;
+      this.totalSessions =
+        todaysAnalyticObj.activeSessions.length +
+        todaysAnalyticObj.closedSessions.length;
+    }
+  }
+
+  const analyticsToSend = new PublicAnalyticObj();
+
+  res.json(analyticsToSend);
+});
+
+router.get("*", (req, res) => {
+  // console.log("404 hit");
+
+  let reqUrl;
+
+  if (req.url.length > 25) {
+    // console.log("req.url longer than 25 chars, truncating");
+
+    for (let i = 1; i < 25; i++) {
+      reqUrl += req.url.charAt(i);
+    }
+  } else {
+    reqUrl = req.url;
+  }
+
+  // const clientIp = requestIp.getClientIp(req); // FOR PROD
+  const clientIp = process.env.TEST_IPADDRESS; // FOR DEV
+
+  logResourceRequest(clientIp, "404 - " + reqUrl);
+
+  res.sendFile(path.join(__dirname, "public", "404.html"));
+});
+
+module.exports = router;
