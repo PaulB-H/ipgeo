03bfeb0b42fc52c401053e073941e1864bc3775d
PaulB-H <51964537+PaulB-H@users.noreply.github.com>
2021-10-15 23:43:24 -0400

    Moved analytic classes, object, and middleware into two separate modules

---
+++ analytic_main.js
@@ -0,0 +1,124 @@
+const _ = require("lodash");
+const Reader = require("@maxmind/geoip2-node").Reader;
+
+class DailyAnalyticObj {
+  constructor() {
+    this.date = Date.now();
+    this.activeSessions = [];
+    this.closedSessions = [];
+    // this.resourceRequests = [];
+    this.iplog = [];
+    this.countries = [];
+  }
+}
+
+let todaysAnalyticObj = new DailyAnalyticObj();
+
+class NewSession {
+  constructor(ip, browser, browserVersion, os, country, city, lat, lon) {
+    this.sessionStartTime = Date.now();
+    this.lastAction = Date.now();
+
+    this.ip = ip;
+    this.browser = browser;
+    this.browserVersion = browserVersion;
+    this.os = os;
+
+    this.sessionRequests = [];
+
+    this.initialScreenWidth = null;
+    this.initialScreenHeight = null;
+    this.country = country;
+    this.city = city;
+    this.lat = lat;
+    this.lon = lon;
+
+    this.sessionEndTime = null;
+  }
+}
+
+class SessionResourceRequest {
+  constructor(url) {
+    this.url = url;
+    this.timeRequested = Date.now();
+  }
+}
+
+class CountryCapture {
+  constructor(country) {
+    this.country = country;
+    this.uniqueHits = 1;
+  }
+}
+
+const createNewSessionAsync = async (clientIp, browser, version, os) => {
+  let country = null;
+  let city = null;
+  let latitude = null;
+  let longitude = null;
+
+  await Reader.open("./GeoLite2-City.mmdb")
+    .then((reader) => {
+      const response = reader.city(clientIp);
+
+      country = response.country.names.en;
+      response.city ? (city = response.city.names.en) : (city = "Not found");
+      latitude = response.location.latitude;
+      longitude = response.location.longitude;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  if (!todaysAnalyticObj.iplog.find((item) => item === clientIp)) {
+    todaysAnalyticObj.iplog.push(clientIp);
+
+    const existingCountryCapture = todaysAnalyticObj.countries.find(
+      (item) => item.country === country
+    );
+    if (!existingCountryCapture) {
+      let newCountryCapture = new CountryCapture(country);
+
+      todaysAnalyticObj.countries.push(newCountryCapture);
+    } else {
+      existingCountryCapture.hits++;
+    }
+  }
+
+  let newSession = new NewSession(
+    clientIp,
+    browser,
+    version,
+    os,
+    country,
+    city,
+    latitude,
+    longitude
+  );
+
+  return newSession;
+};
+
+const logResourceRequest = (clientIp, resource) => {
+  // console.log("logResourceRequest called");
+
+  const existingSession = todaysAnalyticObj.activeSessions.find(
+    (item) => item.ip === clientIp
+  );
+
+  if (existingSession) {
+    let newRequest = new SessionResourceRequest(resource);
+    existingSession.sessionRequests.push(newRequest);
+  }
+
+  // console.log(existingSession);
+};
+
+module.exports = {
+  NewSession,
+  SessionResourceRequest,
+  CountryCapture,
+  todaysAnalyticObj,
+  createNewSessionAsync,
+  logResourceRequest,
+};
---
+++ analytic_middleware.js
@@ -0,0 +1,65 @@
+// Check if session exists for IP in analytic object
+// If false, create new session
+// If true, check if session should be expired
+// --> If expired, close old then create new session
+// --> If not expired, update session last action time
+
+const requestIp = require("request-ip");
+const useragent = require("express-useragent");
+const Reader = require("@maxmind/geoip2-node").Reader;
+
+let { todaysAnalyticObj, createNewSessionAsync } = require("./analytic_main");
+
+module.exports = async function sessionManagement(req, res, next) {
+  const { browser, version, os } = req.useragent;
+
+  // const clientIp = requestIp.getClientIp(req); // FOR PROD
+  const clientIp = process.env.TEST_IPADDRESS; // FOR DEV
+
+  const existingSession = todaysAnalyticObj.activeSessions.find(
+    (item) => item.ip === clientIp
+  );
+
+  if (!existingSession) {
+    let newSession = await createNewSessionAsync(
+      clientIp,
+      browser,
+      version,
+      os
+    );
+
+    todaysAnalyticObj.activeSessions.push(newSession);
+  } else {
+    // console.log("Existing session found");
+
+    let lastAction = existingSession.lastAction;
+    let currTime = Date.now();
+
+    if (currTime - lastAction >= 300000) {
+      // console.log("Session expired, creating new session...");
+
+      let index = todaysAnalyticObj.activeSessions.indexOf(existingSession);
+      let splicedClosedSession = todaysAnalyticObj.activeSessions.splice(
+        index,
+        1
+      );
+      splicedClosedSession[0].sessionEndTime = Date.now();
+      todaysAnalyticObj.closedSessions.push(splicedClosedSession);
+
+      let newSession = await createNewSessionAsync(
+        clientIp,
+        browser,
+        version,
+        os
+      );
+
+      todaysAnalyticObj.activeSessions.push(newSession);
+    } else {
+      // console.log("Session open... Extending");
+
+      existingSession.lastAction = Date.now();
+    }
+  }
+
+  next();
+};
