41ddb67c6edd5b234d1d0a3b75f456ac0f79bb09
PaulB-H <51964537+PaulB-H@users.noreply.github.com>
2021-10-17 00:37:54 -0400

    cron: added dailyBackup job

--- cron.js
+++ cron.js
@@ -4,7 +4,8 @@ const path = require("path");
 
 const { todaysAnalyticObj } = require("./analytic_main");
 
-// crontab.cronhub.io
+// !! ==> Changed to every 5 seconds for dev  <== !! \\
+// crontab.cronhub.io - easy cron creation \\
 const fiveMinBackup = new CronJob("0/5 * * * * *", () => {
   fs.writeFile(
     path.join(__dirname, "analytic_backups", "analytic_data.json"),
@@ -19,3 +20,26 @@ const fiveMinBackup = new CronJob("0/5 * * * * *", () => {
   );
 });
 fiveMinBackup.start();
+
+// 59 seconds past the minute
+// 59 minutes past the hour
+// every 23 hours
+const dailyBackup = new CronJob("59 59 */23 * * *", () => {
+  fs.writeFile(
+    path.join(
+      __dirname,
+      "analytic_backups",
+      `${new Date().toLocaleDateString()}.json`
+    ),
+    JSON.stringify(todaysAnalyticObj),
+    (err) => {
+      if (err) {
+        console.log("Err writing daily backup, exiting");
+        process.exit();
+      }
+      console.log("Daily backup wrote");
+    }
+  );
+});
+
+dailyBackup.start();
