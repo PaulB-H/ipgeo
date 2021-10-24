411e5b09fda5a5de923b5705a591a5a2816d103f
PaulB-H <51964537+PaulB-H@users.noreply.github.com>
2021-10-24 13:54:54 -0400

    Install express-slow-down & add basic implementation within index.js

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const express = require("express");
 const app = express();
 const useragent = require("express-useragent");
 
+const slowDown = require("express-slow-down");
+const speedLimiter = slowDown({
+  windowMs: 10 * 1000, // 10 seconds
+  delayAfter: 10, // allow 10 requests per 10 seconds, then...
+  delayMs: 10, // begin adding 10ms of delay per request above 10:
+  // request # 11 is delayed by 10ms
+  // request # 12 is delayed by 20ms
+  // request # 13 is delayed by 30ms
+});
+app.use(speedLimiter);
+
 require("./backup_loader");
 
 app.use(useragent.express());
