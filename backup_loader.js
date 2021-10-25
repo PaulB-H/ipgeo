d4afd83a4907cc21a16ff8f5139b235e12def13d
PaulB-H <51964537+PaulB-H@users.noreply.github.com>
2021-10-25 01:21:42 -0400

    add public data dir structure

--- backup_loader.js
+++ backup_loader.js
@@ -5,6 +5,8 @@ const { todaysAnalyticObj } = require("./analytic_main");
 const rootBackupDir = path.join(__dirname, "analytic_backups");
 const previousDaysDir = path.join(rootBackupDir, "previous_days");
 const emergencyBackupsDir = path.join(rootBackupDir, "emergency_backups");
+const publicRootDir = path.join(rootBackupDir, "public_data");
+const publicDailyDir = path.join(publicRootDir, "previous_days_public");
 
 if (!fs.existsSync(rootBackupDir)) {
   console.log("analytic_backups dir not found, creating...");
@@ -33,9 +35,15 @@ if (!fs.existsSync(rootBackupDir)) {
     process.exit();
   }
 
-  console.log(
-    `dir structure created:\n  analytic_backups\n    |- previousDays\n    |- emergency_backups`
-  );
+  try {
+    fs.mkdirSync(publicDailyDir);
+  } catch (err) {
+    console.log("Err creating public data dir's, exiting...");
+    console.log(`err.msg: ${err.message}`);
+    process.exit();
+  }
+
+  console.log("Directory structure created");
 } else {
   console.log(`analytic_backups dir already exists...`);
 
@@ -61,6 +69,30 @@ if (!fs.existsSync(rootBackupDir)) {
       process.exit();
     }
     console.log("emergency_backups dir created");
+  }
+
+  if (!fs.existsSync(publicRootDir)) {
+    console.log("public_data dir missing... creating...");
+    try {
+      fs.mkdirSync(publicRootDir);
+    } catch (err) {
+      console.log("Err creating public_data dir, exiting...");
+      console.log(`err.msg: ${err.message}`);
+      process.exit();
+    }
+    console.log("public_data dir created");
+  }
+
+  if (!fs.existsSync(publicDailyDir)) {
+    console.log("public_daily dir missing... creating...");
+    try {
+      fs.mkdirSync(publicDailyDir);
+    } catch (err) {
+      console.log("Err creating public_daily dir, exiting...");
+      console.log(`err.msg: ${err.message}`);
+      process.exit();
+    }
+    console.log("public_daily dir created");
   }
 
   if (!fs.existsSync(path.join(rootBackupDir, "daily_analytics.json"))) {
